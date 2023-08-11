9328f5c9a9555efef68c2113cf1040ad439cd517
anixnoyal <anil.noc@gmail.com>
2023-08-11 12:46:21 +0530

    Create host_filebeat_setup.sh

---
+++ host_filebeat_setup.sh
@@ -0,0 +1,19 @@
+# HOST filebeat and metricbeat installation
+
+Server_name="elk-host1"
+echo "$Server_name" | sudo tee /etc/hostname
+hostnamectl set-hostname $Server_name
+
+yum install filebeat -y
+
+cp /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.original 
+grep -v -e '^[[:space:]]*#' -e '^$' /etc/filebeat/filebeat.yml.original > /etc/filebeat/filebeat.yml
+
+vi /etc/filebeat/filebeat.yml
+output.logstash:
+  hosts: ["your_elasticsearch_server:9200"]
+#or
+  hosts: ["your_logstash_server:5044"]
+
+systemctl enable --now filebeat
+systemctl status filebeat
