56ce8a3ad83ba7d006f3dd4f066bb6e607c57fd4
anixnoyal <anil.noc@gmail.com>
2023-08-14 20:47:38 +0530

    Update aws-sg-create.sh

--- aws-sg-create.sh
+++ aws-sg-create.sh
@@ -2,3 +2,12 @@ aws ec2 create-security-group --group-name ELKStackSG --description "ELK Stack S
 
 
 aws ec2 create-tags --resources sg-0123456789abcdef0 --tags Key=Name,Value=ELKStack Key=Environment,Value=Production
+
+
+
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+INSTANCE_ID=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id`
+echo $INSTANCE_ID
+
+aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" --output text | awk '{print $2 " : " $5}'
+
